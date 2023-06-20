b992347b9f30dace54aaa948b8f2c6ad765d428d
mayur personal <mannu@SIMPL-ENG-060.local>
2023-06-20 17:29:44 +0530

    lld template

---
+++ low-level-design-template/src/main/java/com/mrbajaj/lowleveldesigntemplate/LowLevelDesignTemplateApplication.java
@@ -0,0 +1,16 @@
+package com.mrbajaj.lowleveldesigntemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LowLevelDesignTemplateApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(LowLevelDesignTemplateApplication.class, args);
+
+		System.out.println("BOOOM....... Application Started");
+	}
+
+}
---
+++ low-level-design-template/src/test/java/com/mrbajaj/lowleveldesigntemplate/LowLevelDesignTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrbajaj.lowleveldesigntemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LowLevelDesignTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
