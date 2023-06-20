ec02610ec53e1c465cd65ec01a0779337bfaed4f
mrbajaj <mayurkumarbajaj@gmail.com>
2023-06-21 02:57:54 +0530

    cli template updates

--- lld-cli-template/src/main/java/org/mrbajaj/Main.java
+++ lld-cli-template/src/main/java/org/mrbajaj/Main.java
@@ -1,7 +1,15 @@
 package org.mrbajaj;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int option = 1;
+        do{
+            System.out.println("Press 0 to stop, any other integer to continue.!!");
+            option = sc.nextInt();
+        }while(option != 0);
 
         System.out.println("Hello world!");
     }
