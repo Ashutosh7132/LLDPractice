1fd22d7df17a98279031bdf4ad8bc85649da3708
mrbajaj <mayurkumarbajaj@gmail.com>
2023-06-21 13:37:34 +0530

    added pom dependencies

--- lld-cli-template/src/main/java/org/mrbajaj/Main.java
+++ lld-cli-template/src/main/java/org/mrbajaj/Main.java
@@ -5,12 +5,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("Hello world!");
+
         int option = 1;
         do{
             System.out.println("Press 0 to stop, any other integer to continue.!!");
             option = sc.nextInt();
         }while(option != 0);
 
-        System.out.println("Hello world!");
+
+        System.out.println("Bye Bye");
     }
 }
